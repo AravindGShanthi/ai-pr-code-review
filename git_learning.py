4b1ad033dacea3ffb09a7053274635bb57567d14
Aravind Gunasekaran <aravindgshanthi@gmail.com>
2025-12-05 23:13:23 +0530

    First commit

---
+++ git_learning.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from github import Github
+import os
+from github import Auth
+
+load_dotenv()
+
+auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
+
+a = Github(auth=auth)
+
+for repo in a.get_user().get_repos():
+    print(repo.name)
+    # print(dir(repo))
+
+
+a.close()
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Any
+
+
+class WebhookPayload(BaseModel):
+    # zen: str
+    action: str
+    repository: dict[str, str]
+    pull_request: dict[str, str]
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def helloWorld():
+    return {"hello": "world"}
+
+
+@app.post("/webhook/pr")
+async def handle_pr(pr: WebhookPayload):
+    print(pr)
+    if pr.action not in ["opened", "synchronize", "reopened"]:
+        return {"status": "ignored"}
+
+    repo_full = pr.repository["full_name"]
+    pr_number = pr.pull_request["number"]
+
+    return {
+        "status": "processing",
+        "data": {"repo_full": repo_full, "pr_number": pr_number},
+        "complete_data": pr,
+    }
